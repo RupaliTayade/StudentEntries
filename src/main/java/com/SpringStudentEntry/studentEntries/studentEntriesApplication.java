995e474ffe1d678d3c38d081e086d6e1ad16fb85
Rupali Pravin Tayade <rupali.pravin.tayade@accenture.com>
2020-05-25 16:37:37 +0300

    Add README.md file and arrange test package structure.

---
+++ src/main/java/com/SpringStudentEntry/studentEntries/studentEntriesApplication.java
@@ -0,0 +1,13 @@
+package com.SpringStudentEntry.studentEntries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class studentEntriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(studentEntriesApplication.class, args);
+	}
+
+}
